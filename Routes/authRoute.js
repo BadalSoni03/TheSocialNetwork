349ce871b7d2c93f81521ec80591ad345ac034df
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-12-15 00:09:36 +0530

    Refactored

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -12,30 +12,16 @@ const {
 } = require('../Controllers/authController');
 
 
-/*-------------------------------POST APIS-------------------------------*/
+/*-------------------------------POST APIs-------------------------------*/
 
 
-/*
-	@desc : Registers the user in the application
-	@API : Public
-	@method : post
-*/
 router.post('/register' , registerValidation , userValidation , registerController);
 
 
-/*
-	@desc : Logs in the user in the application
-	@API : Public
-	@method : post
-*/
 router.post('/login' , loginValidation , userValidation , loginController);
 
 
-/*
-	@desc : Logs out the user from the application
-	@API : Public
-	@method : post
-*/
 router.post('/logout' , isAuth , logoutController);
+
 
 module.exports = router;

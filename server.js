5ad8b55ac2da09bcdd0934916fbd7086d1bc4ec1
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-12-15 00:13:26 +0530

    post route added

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const cloudinary = require('cloudinary');
+const fileUpload = require('express-fileupload');
 const connectDB = require('./db');
-const authRouter = require('./Routes/authRoute');
-const userRouter = require('./Routes/userRoute');
 
+// database connection
 connectDB();
 
 const app = express();
 const port = 3000 || process.env.PORT;
-const host = process.env.HOST;
+const host = process.env.HOST || 'localhost';
 
-
+// middlewares
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(fileUpload({
+	useTempFiles : true
+}));
+
+
+// routes
+const authRouter = require('./Routes/authRoute');
+const userRouter = require('./Routes/userRoute');
+const postRouter = require('./Routes/postRoute');
+
 app.use('/api/auth' , authRouter);
 app.use('/api/user' , userRouter);
+app.use('/api/post' , postRouter);
 
 
 app.listen(port , () => {

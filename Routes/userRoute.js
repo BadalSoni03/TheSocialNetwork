86950b9221938fa32d24344cf826dd36488385b0
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-12-15 00:10:49 +0530

    Some routes added

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -14,138 +14,82 @@ const {
 	approveFollowRequestsController,
 	rejectFollowRequestsController,
 	viewFollowRequestsController,
-	getMutualAccountsController
+	getMutualAccountsController,
+	removeFollowerController,
+	muteUserController,
+	getMutedUsersController,
+	unmuteUserController,
+	editProfilePicController
 } = require('../Controllers/userController');
 
 
 /*-------------------------------POST APIs-------------------------------*/
 
 
-/*
-	@desc : Follows the user with id 'userID'
-	@API : Protected Public
-	@method : post
-	@note : If the account is private then the request is pushed into the stack of the followRequests of the user
-*/
 router.post('/follow/:userID' , isAuth , followUserController);
 
 
-/*
-	@desc : Unfollows the user with id 'userID'
-	@API : Protected Public
-	@method : post
-*/
 router.post('/unfollow/:userID' , isAuth , unfollowUserController);
 
 
-/*
-	@desc : Disables the account of the logged in user
-	@API : Protected Public
-	@method : post
-*/
-router.post('/account/disable' , isAuth , disableAccountController);
-
-
-/*
-	@desc : Enables the account of the logged in user
-	@API : Protected Public
-	@method : post
-*/
-router.post('/account/enable' , isAuth , enableAccountController);
-
-
-/*
-	@desc : Blocks the user with id 'userID'
-	@API : Protected Public
-	@method : post
-*/
 router.post('/block/:userID' , isAuth , blockUserController);
 
 
-/*
-	@desc : Unblocks the user with id 'userID'
-	@API : Protected Public
-	@method : post
-*/
 router.post('/unblock/:userID' , isAuth , unblockUserController);
 
 
-/*
-	@desc :  Approves the follow requests
-	@API : Protected Public
-	@method : post
-*/
 router.post('/follow/requests/approve' , isAuth , approveFollowRequestsController);
 
 
-/*
-	@desc :  Rejects the follow requests
-	@API : Protected Public
-	@method : post
-*/
 router.post('/follow/requests/reject' , isAuth , rejectFollowRequestsController);
+
+
+router.post('/account/followers/remove/:userID' , isAuth , removeFollowerController);
+
+
+router.post('/mute/:userID' , isAuth , muteUserController);
+
+
+router.post('/unmute/:userID' , isAuth , unmuteUserController);
 
 
 /*--------------------------------GET APIs-------------------------------*/
 
 
-/*
-	@desc : Gets the profile of the user with id 'userID'
-	@API : Protected Public
-	@method : get
-*/
 router.get('/profile/:userID' , isAuth , viewProfileController);
 
 
-/*
-	@desc : Gets the profile of the user from req.body
-	@API : Protected Public
-	@method : get
-*/
 router.get('/profile' , isAuth , viewProfileController);
 
 
-/*
-	@desc : Gets the followings of the user with id 'userID'
-	@API : Protected Public
-	@method : get
-*/
 router.get('/profile/followings/:userID' , isAuth , viewFollowingsController);
 
 
-/*
-	@desc : Gets the followers of the user with id 'userID'
-	@API : Protected Public
-	@method : get
-*/
 router.get('/profile/followers/:userID' , isAuth , viewFollowersController);
 
 
-/*
-	@desc : Gets the followerRequests of the user
-	@API : Protected Public
-	@method : get
-*/
 router.get('/account/follow-requests' , isAuth , viewFollowRequestsController);
 
 
-/*
-	@desc : Gets the mutual friends of a user randomly
-	@API : Protected Public
-	@method : get
-*/
 router.get('/account/mutuals' , isAuth , getMutualAccountsController);
 
 
-/*--------------------------------PUT APIs-------------------------------*/
+router.get('/account/muted-users' , isAuth , getMutedUsersController);
 
 
-/*
-	@desc : Edits the profile of the user
-	@API : Protected Public
-	@method : put
-*/
-router.put('/account/edit' , isAuth , editProfileController);
+/*--------------------------------PATCH APIs-------------------------------*/
+
+
+router.post('/account/disable' , isAuth , disableAccountController);
+
+
+router.post('/account/enable' , isAuth , enableAccountController);
+
+
+router.patch('/account/edit' , isAuth , editProfileController);
+
+
+router.patch('/account/edit/profile-pic' , isAuth , editProfilePicController);
 
 
 module.exports = router;
